6b472e79704ffaa743bc04b079bf9d343ec2c602
benchel <whalswl1030@nate.com>
2023-11-13 17:52:13 +0900

    db poll 생성 추가

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql');
+
 const app = express();
 const port = process.env.PORT || 8033;
 
@@ -11,3 +13,13 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`server is listening at localhost:${process.env.PORT}`);
 });
+
+// DB poll 생성 
+export const db = mysql.createPool({
+    host : '', // 호스트명 
+    user : '', // 사용자
+    password : '', // 비밀번호
+    connectionLimit : '' // 연결제한수
+});
+
+
